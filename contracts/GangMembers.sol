aa63c598d5c150b6d51834b5bbf82e4aea7db2ce
thugtiki <dick.cheney@tutanota.com>
2022-01-16 12:26:01 +0200

    add member functionality

--- contracts/GangMembers.sol
+++ contracts/GangMembers.sol
@@ -1,33 +1,56 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.3;
 
 // import "@openzeppelin/contracts/access/Ownable.sol";
 
 import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721URIStorageUpgradeable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
 
-contract GangMembers is Initializable, ERC721Upgradeable, ERC721URIStorageUpgradeable  {
+contract GangMembers is Initializable, ERC721Upgradeable, ERC721URIStorageUpgradeable  { 
+    using Counters for Counters.Counter;
+    Counters.Counter private _memberIdCounter;
+
     address public gangFactory;
+    uint8 constant maxMembers = 12;
+    
+    mapping(uint => address) idToMember;
 
-    uint8 public maxMembers;
-
-    function initialize(string calldata name, string calldata symbol, uint8 members) initializer public {
+    function initialize(string calldata name, string calldata symbol) initializer public {
       __ERC721_init_unchained(name, symbol);
-      maxMembers = members;
+      __ERC721URIStorage_init(); // not sure if 100% necessar
 
       // maybe mint everything in here or too much gas? 
     }
 
+    function mint(address to) public returns (uint) {
+        uint currentId = _memberIdCounter.current();
+        require(currentId <= maxMembers, "gang is full, join another one!");
+
+       
+        _mint(to, currentId);
+        idToMember[currentId] = to;
+        _memberIdCounter.increment();
+
+        return currentId;
+    }
     
-    
-    // not sure why these need to be overridden
+    // TODO remove many of the unnecessary string memories being passed around, expensive
+    function setTokenURI(uint tokenId, string memory metadataURI) external returns (string memory) {
+        require(ownerOf(tokenId) == msg.sender, "you don't own that nft baby");
+
+        _setTokenURI(tokenId, metadataURI);
+        return metadataURI;
+    }
+
+
 
     function _burn(uint256 tokenId) internal override(ERC721Upgradeable, ERC721URIStorageUpgradeable)
     {
         _burn(tokenId);
     }
-
 
     function tokenURI(uint256 tokenId) public view override(ERC721Upgradeable, ERC721URIStorageUpgradeable)
         returns (string memory)
@@ -36,3 +59,8 @@ contract GangMembers is Initializable, ERC721Upgradeable, ERC721URIStorageUpgrad
     }
  
 }
+
+/*
+TODO 
+    - add tokenURI setting
+*/
