72c954845c95cc6a862177ab62e09e163cee6754
thugtiki <dick.cheney@tutanota.com>
2022-01-16 14:36:20 +0200

    webpack fix for web3-ui

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { NETWORKS, Provider } from '@web3-ui/core'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider network={NETWORKS.goerli}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
